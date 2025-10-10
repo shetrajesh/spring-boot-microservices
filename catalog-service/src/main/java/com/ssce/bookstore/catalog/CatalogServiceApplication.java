071639e46728cdec86989dbb0b44bcbcc46d17c9
rshet007 <rshet007@yahoo.com>
2025-10-10 22:58:50 +0530

    Run CI/CD - 10-10-2025 22.58

--- catalog-service/src/main/java/com/ssce/bookstore/catalog/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/ssce/bookstore/catalog/CatalogServiceApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @ConfigurationPropertiesScan
 public class CatalogServiceApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(CatalogServiceApplication.class, args);
     }
